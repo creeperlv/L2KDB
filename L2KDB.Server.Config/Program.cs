c7594e43c80912901fdfe1d57459896d2b368e55
Creeper Lv <1376021184@qq.com>
2019-10-17 22:43:11 +0800

    Doing Server Config Tool.

---
+++ L2KDB.Server.Config/Program.cs
@@ -0,0 +1,71 @@
+﻿using LiteDatabase;
+using System;
+
+namespace L2KDB.Server.Config
+{
+    class Program
+    {
+        static void MainScene()
+        {
+            Console.Clear();
+            Console.WriteLine("L2KDB - Local 2-Key Database");
+            Console.WriteLine("Set An Administrator");
+            Console.WriteLine("Enter user name:");
+            var usr = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            var pwd = Console.ReadLine();
+            database.OpenForm("");
+        }
+        static Database database = new Database("./Server-Config");
+        static void SetAdmin()
+        {
+            Console.Clear();
+            Console.WriteLine("L2KDB - Local 2-Key Database");
+            Console.WriteLine("Server Configure");
+            Console.WriteLine("1 - Add An Administrator");
+            Console.WriteLine("2 - Add An Permission to An AuthID");
+            Console.WriteLine("3 - Add Permission 'FullDBAccess' to An AuthID");
+            Console.WriteLine("4 - Set IP address will be used in next launch");
+            var option=Console.ReadLine();
+            try
+            {
+                var targetScene = int.Parse(option);
+                if (targetScene < 4 & targetScene > 0)
+                {
+                    scene = targetScene;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        static int scene = 0;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                switch (scene)
+                {
+                    case 0:
+                        {
+                            MainScene();
+                        }
+                        break;
+                    case 1:
+                        {
+                            SetAdmin();
+                        }
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Undefined Scene!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        scene = 0;
+                        break;
+                }
+
+            }
+        }
+    }
+}
